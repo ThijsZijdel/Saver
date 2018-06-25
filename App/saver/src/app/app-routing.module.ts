10e1081b2884f7ee28f0e1cd4b5e458944624408
Thijs <Thijs.zijdel@gmail.com>
2018-06-25 04:58:40 +0200

    Routes

--- App/saver/src/app/app-routing.module.ts
+++ App/saver/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PersonalComponent} from "./views/personal/personal.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: PersonalComponent },
+  // { path: 'current/:id', component: CurrentTaskComponent },
+  {path: '404', component: PersonalComponent},
+  {path: '**', redirectTo: '/404'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
