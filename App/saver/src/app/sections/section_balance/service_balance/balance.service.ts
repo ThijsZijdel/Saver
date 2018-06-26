b321bc2692088242cefb4866154f2ccf85a4c604
Thijs <Thijs.zijdel@gmail.com>
2018-06-26 18:03:42 +0200

    Balance + Expenses - Service data layers

--- App/saver/src/app/sections/section_balance/service_balance/balance.service.ts
+++ App/saver/src/app/sections/section_balance/service_balance/balance.service.ts
@@ -1,9 +1,149 @@
 import { Injectable } from '@angular/core';
+import { catchError, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+import {Observable, of} from 'rxjs/index';
+import {Balance} from "../../../models/Balance";
+import {MessageService} from "../../../data/service_message/message.service";
+
+
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
 export class BalanceService {
 
-  constructor() { }
+  private balanceUrl = 'api/balances';  // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  /**
+   * get balances from the server
+   * @author Thijs Zijdel
+   */
+  getBalances(): Observable<Balance[]> {
+    const url = `${this.balanceUrl}?action=getAll`;
+    return this.http.get<Balance[]>(this.balanceUrl)
+      .pipe(
+        tap(balances => this.log(`fetched balances`)),
+        catchError(this.handleError('getBalance', []))
+      );
+  }
+
+  /**
+   * get balance by id.
+   * @author Thijs Zijdel
+   */
+  getBalance(id: number): Observable<Balance> {
+    const url = `${this.balanceUrl}?action=get&id=${id}`;
+
+    return this.http.get<Balance>(url).pipe(
+      tap(_ => this.log(`fetched balance id=${id}`)),
+      catchError(this.handleError<Balance>(`getBalance id=${id}`))
+    );
+  }
+
+  getBalancesOf(month: number, year: number): Observable<Balance[]> {
+    const url = `${this.balanceUrl}?action=getBalances&month=${month}&year=${year}`;
+
+    return this.http.get<Balance[]>(url).pipe(
+      tap(_ => this.log(`fetched Balances month=${month} year=${year}`)),
+      catchError(this.handleError<Balance[]>(`getBalances month=${month} year=${year}`))
+    );
+  }
+
+  /**
+   * Log a balanceservice message with the MessageService
+   * @author Thijs Zijdel
+   */
+  private log(message: string) {
+    this.messageService.add('BalanceService: ' + message);
+  }
+
+
+  /**
+   * Handle Http operation that failed.
+   *
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   * @author Thijs Zijdel
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.error(error); // log to console
+
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Prevent crashing the application
+      return of(result as T);
+    };
+  }
+
+
+  /**
+   * update the balance on the server
+   * note: PUT
+   * @author Thijs Zijdel
+   */
+  updateBalance (balance: Balance): Observable<any> {
+    const url = `${this.balanceUrl}?action=edit`;
+
+    return this.http.put(url, balance, httpOptions).pipe(
+      tap(_ => this.log(`updated balance id=${balance.id}`)),
+      catchError(this.handleError<any>('updateBalance'))
+    );
+  }
+
+  /**
+   * add a new balance to the server
+   * note: POST
+   */
+  addBalance (balance: Balance): Observable<Balance> {
+    const url = `${this.balanceUrl}?action=add`;
+
+    return this.http.post<Balance>(url, balance, httpOptions).pipe(
+      tap((balance: Balance) => this.log(`added balance w/ id=${balance.id}`)),
+      catchError(this.handleError<Balance>('addBalance'))
+    );
+  }
+
+  /**
+   * delete the balance from the server
+   * note: DELETE
+   * @author Thijs Zijdel
+   */
+  deleteBalance (balance: Balance | number): Observable<Balance> {
+    const id = typeof balance === 'number' ? balance : balance.id;
+    const url = `${this.balanceUrl}?action=delete&id=${id}`;
+
+    return this.http.delete<Balance>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted incom id=${id}`)),
+      catchError(this.handleError<Balance>('deleteBalance'))
+    );
+  }
+
+  /**
+   * get balances whose name contains search term
+   * note: GET
+   * @author Thijs Zijdel
+   */
+  searchBalances(term: string): Observable<Balance[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Balance[]>(`api/balances/?name=${term}`).pipe(
+      tap(_ => this.log(`found balances matching "${term}"`)),
+      catchError(this.handleError<Balance[]>('searchBalances', []))
+    );
+  }
+
 }
+
--- App/saver/src/app/sections/section_expense/service_expense/expense.service.ts
+++ App/saver/src/app/sections/section_expense/service_expense/expense.service.ts
@@ -1,9 +1,149 @@
 import { Injectable } from '@angular/core';
+import { catchError, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+import {Observable, of} from 'rxjs/index';
+import {Expense} from "../../../models/Expense";
+import {MessageService} from "../../../data/service_message/message.service";
+
+
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
 export class ExpenseService {
 
-  constructor() { }
+  private expensUrl = 'api/expenses';  // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  /**
+   * get expenses from the server
+   * @author Thijs Zijdel
+   */
+  getExpenses(): Observable<Expense[]> {
+    const url = `${this.expensUrl}?action=getAll`;
+    return this.http.get<Expense[]>(this.expensUrl)
+      .pipe(
+        tap(expenses => this.log(`fetched expenses`)),
+        catchError(this.handleError('getExpense', []))
+      );
+  }
+
+  /**
+   * get expense by id.
+   * @author Thijs Zijdel
+   */
+  getExpense(id: number): Observable<Expense> {
+    const url = `${this.expensUrl}?action=get&id=${id}`;
+
+    return this.http.get<Expense>(url).pipe(
+      tap(_ => this.log(`fetched expense id=${id}`)),
+      catchError(this.handleError<Expense>(`getExpense id=${id}`))
+    );
+  }
+
+  getExpensesOf(month: number, year: number): Observable<Expense[]> {
+    const url = `${this.expensUrl}?action=getExpenses&month=${month}&year=${year}`;
+
+    return this.http.get<Expense[]>(url).pipe(
+      tap(_ => this.log(`fetched Expenses month=${month} year=${year}`)),
+      catchError(this.handleError<Expense[]>(`getExpenses month=${month} year=${year}`))
+    );
+  }
+
+  /**
+   * Log a expenseservice message with the MessageService
+   * @author Thijs Zijdel
+   */
+  private log(message: string) {
+    this.messageService.add('ExpenseService: ' + message);
+  }
+
+
+  /**
+   * Handle Http operation that failed.
+   *
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   * @author Thijs Zijdel
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.error(error); // log to console
+
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Prevent crashing the application
+      return of(result as T);
+    };
+  }
+
+
+  /**
+   * update the expense on the server
+   * note: PUT
+   * @author Thijs Zijdel
+   */
+  updateExpense (expense: Expense): Observable<any> {
+    const url = `${this.expensUrl}?action=edit`;
+
+    return this.http.put(url, expense, httpOptions).pipe(
+      tap(_ => this.log(`updated expense id=${expense.id}`)),
+      catchError(this.handleError<any>('updateExpense'))
+    );
+  }
+
+  /**
+   * add a new expense to the server
+   * note: POST
+   */
+  addExpense (expense: Expense): Observable<Expense> {
+    const url = `${this.expensUrl}?action=add`;
+
+    return this.http.post<Expense>(url, expense, httpOptions).pipe(
+      tap((expense: Expense) => this.log(`added expense w/ id=${expense.id}`)),
+      catchError(this.handleError<Expense>('addExpense'))
+    );
+  }
+
+  /**
+   * delete the expense from the server
+   * note: DELETE
+   * @author Thijs Zijdel
+   */
+  deleteExpense (expense: Expense | number): Observable<Expense> {
+    const id = typeof expense === 'number' ? expense : expense.id;
+    const url = `${this.expensUrl}?action=delete&id=${id}`;
+
+    return this.http.delete<Expense>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted incom id=${id}`)),
+      catchError(this.handleError<Expense>('deleteExpense'))
+    );
+  }
+
+  /**
+   * get expenses whose name contains search term
+   * note: GET
+   * @author Thijs Zijdel
+   */
+  searchExpenses(term: string): Observable<Expense[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Expense[]>(`api/expenses/?name=${term}`).pipe(
+      tap(_ => this.log(`found expenses matching "${term}"`)),
+      catchError(this.handleError<Expense[]>('searchExpenses', []))
+    );
+  }
+
 }
+
