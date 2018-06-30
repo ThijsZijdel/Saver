7ba7a59876cd21ee876c5deba18a8c8b5839457a
Thijs <Thijs.zijdel@gmail.com>
2018-06-30 13:34:30 +0200

    Dynamic calendar + created& implemented

--- App/saver/src/app/sections/section_bills/calendar/calendar.component.ts
+++ App/saver/src/app/sections/section_bills/calendar/calendar.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import * as $ from "jquery"
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
@@ -10,152 +11,152 @@ export class CalendarComponent implements OnInit {
 
   constructor() { }
 
+  month: string;
+  year: number = new Date().getFullYear();
+  currentMonth: number = new Date().getMonth() ;
+
   ngOnInit() {
-    var MONTH_NAMES = 'January February March April May June July August September October November December'.split(' ');
+    this.setUpCalendar(this.currentMonth);
+  }
+
+  setUpCalendar(monthDisplay: number){
+    const MONTH_NAMES = 'January February March April May June July August September October November December'.split(' ');
 
     // for (var i = 0; i < 12; i++) {
-    var i = 5;
+    let i = monthDisplay
 
     // create a table for month with thead & tbody
-    var div = document.createElement('div');
-    var p = document.createElement('p');
-    var table = document.createElement('table');
-    var thead = document.createElement('thead');
-    var tbody = document.createElement('tbody');
+    let div = document.createElement('div');
+    let p = document.createElement('p');
+    let table = document.createElement('table');
+    let thead = document.createElement('thead');
+    let tbody = document.createElement('tbody');
 
     // get weeks for a given month
-    var calendar = createCalendar(2018, i);
+    let calendar = this.createCalendar(this.year, i);
 
     //get current date
-    var currentDate = new Date();
+    let currentDate = new Date();
 
     // iterate weeks
-    for (var j = 0; j < calendar.length; j++) {
+    for (let j = 0; j < calendar.length; j++) {
       // create table row for every week
-      var tr = $('table#calendarTbl tr#'+j)// tr get
-      console.log('tr: table#calendarTbl tr#'+j);
+      let tr = document.createElement('tr');
 
-      var week = calendar[j];
+      let week = calendar[j];
 
       // iterate days
-      for (var k = 0; k < week.length; k++) {
+      for (let k = 0; k < week.length; k++) {
         // create table cell for every day
-        var td = $('table#calendarTbl tr#'+j+' td:nth-child('+k+')');
-        console.log('table#calendarTbl tr#'+j+' td:nth-child('+k+')');
-        var day = week[k];
+        let td = document.createElement('td');
+
+        let day = week[k];
 
         // set day of month as table cell text content
         td.textContent = day.date.getDate();
 
         // add before/after class
-        day.before && td.addClass('before');
-        day.after && td.addClass('after');
+        day.before && td.classList.add('before');
+        day.after && td.classList.add('after');
 
-        // check for current date
-        if (currentDate == day.date.getDate()){
-          // add styling class
-          td.addClass('currentDay');
-          day.currentDay && td.addClass('currentDay');
-        }
+        if (!day.before && !day.after && i == new Date().getMonth())
+          day.current && td.classList.add('current');
 
-        td.html()
-        // mount table cell
+
         tr.appendChild(td);
       }
+
       // mount table row
       tbody.appendChild(tr);
       // z}
 
-
-
-
       // create month name
-      p.textContent = MONTH_NAMES[i];
+      this.month = MONTH_NAMES[i];
 
       // create thead
       thead.innerHTML = '<tr><td>' + 'Su Mo Tu We Th Fr Sa'.split(' ').join('</td><td>') + '</td></tr>';
+
+      // set class on table
+      table.classList.add('calendarTbl');
 
       // mount thead & tbody
       table.appendChild(thead);
       table.appendChild(tbody);
 
-      $('table#calendarTbl thead').html(thead)
-      $('table#calendarTbl tbody').html(tbody)
 
+      // mount table to container
+      div.appendChild(table);
 
+      // mount table to body
+      $('div#calender-box').html(div);
 
-      // // mount month name to container
-      // div.appendChild(p);
-      //
-      // // mount table to container
-      // div.appendChild(table);
-      //
-      // // mount table to body
-      // // $('#calender-box').html(div);
-      //
-      // // styling
-      //
-      // // class
-      // //$('#calender-box div table').addClass('tableData');
     }
 
-    function createCalendar (year, month) {
-      var results = [];
+  }
 
-      // find out first and last days of the month
-      var firstDate = new Date(year, month, 1);
-      var lastDate = new Date(year, month + 1, 0)
+  createCalendar (year, month) {
+    let results = [];
 
-      // calculate first sunday and last saturday
-      var firstSunday = getFirstSunday(firstDate);
-      var lastSaturday = getLastSaturday(lastDate);
+    // find out first and last days of the month
+    let firstDate = new Date(year, month, 1);
+    let lastDate = new Date(year, month + 1, 0)
 
-      // iterate days starting from first sunday
-      var iterator = new Date(firstSunday);
-      var i = 0;
+    // calculate first sunday and last saturday
+    let firstSunday = this.getFirstSunday(firstDate);
+    let lastSaturday = this.getLastSaturday(lastDate);
 
-      // ..until last saturday
-      while (iterator <= lastSaturday) {
-        if (i++ % 7 === 0) {
-          // start new week when sunday
-          var week = [];
-          results.push(week);
-        }
+    // iterate days starting from first sunday
+    let iterator = new Date(firstSunday);
+    let i = 0;
 
-        // push day to week
-        week.push({
-          date: new Date(iterator),
-          before: iterator < firstDate, // add indicator if before current month
-          after: iterator > lastDate // add indicator if after current month
-        });
-
-        // iterate to next day
-        iterator.setDate(iterator.getDate() + 1);
+    // ..until last saturday
+    while (iterator <= lastSaturday) {
+      if (i++ % 7 === 0) {
+        // start new week when sunday
+        var week = [];
+        results.push(week);
       }
 
-      return results;
+      // push day to week
+      week.push({
+        date: new Date(iterator),
+        before: iterator < firstDate, // add indicator if before current month
+        after: iterator > lastDate, // add indicator if after current month
+        current: iterator.getDate() == new Date().getDate() // check for current date
+      });
+
+      // iterate to next day
+      iterator.setDate(iterator.getDate() + 1);
     }
 
-    function getFirstSunday (firstDate) {
-      var offset = firstDate.getDay();
+    return results;
+  }
 
-      var result = new Date(firstDate);
-      result.setDate(firstDate.getDate() - offset);
+  private getFirstSunday (firstDate): Date {
+    let offset = firstDate.getDay();
 
-      return result;
-    }
+    let result = new Date(firstDate);
+    result.setDate(firstDate.getDate() - offset);
 
-    function getLastSaturday (lastDate) {
-      var offset = 6 - lastDate.getDay();
+    return result;
+  }
 
-      var result = new Date(lastDate);
-      result.setDate(lastDate.getDate() + offset);
+  private getLastSaturday (lastDate): Date {
+    let offset = 6 - lastDate.getDay();
 
-      return result;
-    }
+    let result = new Date(lastDate);
+    result.setDate(lastDate.getDate() + offset);
+
+    return result;
   }
 
 
+  changeMonth(number: number) {
+    //todo check year
+    this.currentMonth = this.currentMonth + number;
+
+    this.setUpCalendar(this.currentMonth);
+  }
 }
 // display: inline-block;
 // vertical-align: top;
