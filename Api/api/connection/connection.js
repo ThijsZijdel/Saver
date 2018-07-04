f7d5eb7f18234eb2863a34e7fdf733fa5e560879
Thijs <Thijs.zijdel@gmail.com>
2018-07-04 16:43:52 +0200

    Connection started

--- Api/api/connection/connection.js
+++ Api/api/connection/connection.js
@@ -1,16 +1,43 @@
-let mysql      = require('mysql');
-
+let mysql      = require('mysql'),
+    express    = require('express'),
+    router     = express.Router();
 
 let connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : 'root'
+    password : 'root',
+    database : 'SaveSome'
 });
 
-connection.connect();
+// Connect & Catch errors
+connection.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySql DB connected')
+});
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-    if (err) throw err;
+
+router.get('/createDB', (req, res) => {
+    let sql = "CREATE DATABASE SaveSome";
+
+    connection.query(sql, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        console.log(result);
+        res.send('Database created');
+    });
+});
+
+
+
+
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+    if (err){
+        throw err
+    };
     console.log('The solution is: ', rows[0].solution);
 });
 
@@ -19,15 +46,11 @@ connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
 
 
 
-function getConnection(){
-    connection.connect();
-
-    return connection;
-}
 
 
 
 connection.end();
 
+module.exports = router;
 
 // module.exports = getConnection();
