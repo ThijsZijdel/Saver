6cd39dd202b561cb3a3933a67e2e0a116a79976d
Thijs <Thijs.zijdel@gmail.com>
2018-07-25 19:18:40 +0200

    Add : manage company (/add)

---
+++ App/saver/src/app/data/manage/components/manage-company/manage-company.component.ts
@@ -0,0 +1,58 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {CompanyService} from "../../../service/service_company/company.service";
+import {Company} from "../../../../models/Company";
+import {AddViewsService} from "../../addViews/service_addViews/addViews.service";
+import {Category} from "../../../../models/Category";
+import {CategoryService} from "../../../categories/service_category/category.service";
+
+@Component({
+  selector: 'app-manage-company',
+  templateUrl: './manage-company.component.html',
+  styleUrls: ['./manage-company.component.css']
+})
+export class ManageCompanyComponent implements OnInit {
+
+   company: Company = null;
+
+  companies: Company[] = null;
+
+  categories: Category[] = [];
+
+  constructor(protected addView: AddViewsService,
+              private serviceCompanies: CompanyService,
+              private serviceCategories: CategoryService) { }
+
+  ngOnInit() {
+    this.company = this.addView.getCompany();
+    this.companies = this.getCompanies();
+  }
+
+
+  private getCategories(){
+    this.categories = [];
+
+    this.serviceCategories.getCategories().subscribe(categories => {
+      for (let category of categories){
+        this.categories.push(category);
+      }
+    });
+  }
+
+  private getCompanies() {
+
+    let data: Company[] = [];
+
+    this.serviceCompanies.getCompanies().subscribe(companies => {
+      // loop trough all the categories
+      for (let company of companies) {
+        data.push(company);
+
+        if(company.subCategoryFk != 0){
+
+        }
+      }
+    });
+    return data;
+  }
+
+}
