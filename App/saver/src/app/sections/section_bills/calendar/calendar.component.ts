d29a1abfab0ce651aafa440dd5f66fd90f6e7dec
Thijs <Thijs.zijdel@gmail.com>
2018-08-14 16:43:46 +0200

    Fix: dynamic calendar - logic

--- App/saver/src/app/sections/section_bills/calendar/calendar.component.ts
+++ App/saver/src/app/sections/section_bills/calendar/calendar.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import * as $ from "jquery"
+import {ExpenseService} from "../../section_expense/service_expense/expense.service";
+import {Expense} from "../../../models/Expense";
 
 @Component({
   encapsulation: ViewEncapsulation.None,
@@ -9,7 +11,7 @@ import * as $ from "jquery"
 })
 export class CalendarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private serviceExpenses: ExpenseService) { }
 
   today = new Date();
   month: string;
@@ -18,7 +20,9 @@ export class CalendarComponent implements OnInit {
   displayedMonth: number = this.today.getMonth() ;
 
   ngOnInit() {
-    this.setUpCalendar(this.currentMonth);
+    this.initCalendar();
+
+
   }
 
   setUpCalendar(monthDisplay: number){
@@ -29,7 +33,6 @@ export class CalendarComponent implements OnInit {
 
     // create a table for month with thead & tbody
     let div = document.createElement('div');
-    let p = document.createElement('p');
     let table = document.createElement('table');
     let thead = document.createElement('thead');
     let tbody = document.createElement('tbody');
@@ -65,12 +68,15 @@ export class CalendarComponent implements OnInit {
           day.current && td.classList.add('current');
 
 
+        day.paid && td.classList.add('paid');
+        day.bill && td.classList.add('bill');
+        day.over && td.classList.add('over');
+
         tr.appendChild(td);
       }
 
       // mount table row
       tbody.appendChild(tr);
-      // z}
 
       // create month name
       this.month = MONTH_NAMES[i];
@@ -119,12 +125,19 @@ export class CalendarComponent implements OnInit {
         results.push(week);
       }
 
+      let obj = this.getInfObj(iterator.toISOString().slice(0,10));
+
+
+
       // push day to week
       week.push({
         date: new Date(iterator),
         before: iterator < firstDate, // add indicator if before current month
         after: iterator > lastDate, // add indicator if after current month
-        current: iterator.getDate() == new Date().getDate() // check for current date
+        current: iterator.getDate() == new Date().getDate(), // check for current date
+        bill: obj.bill,
+        over: obj.over,
+        paid: obj.paid
       });
 
       // iterate to next day
@@ -154,7 +167,7 @@ export class CalendarComponent implements OnInit {
 
 
   changeMonth(interval: number) {
-    if (interval == null){
+    if (interval === null){
       this.displayedMonth = this.today.getMonth();
       this.year = this.today.getFullYear();
     }
@@ -182,7 +195,7 @@ export class CalendarComponent implements OnInit {
 
     //Else just emit the change
     this.emitMonth(interval);
-    this.setUpCalendar(this.displayedMonth);
+    this.initCalendar();
   }
 
   /**
@@ -193,11 +206,12 @@ export class CalendarComponent implements OnInit {
     //Set the month
     this.displayedMonth = this.displayedMonth + interval;
 
-    this.setUpCalendar(this.displayedMonth);
-
+    this.initCalendar();
     //Emit it to the Event
     // this.change.emit(this.month);
   }
+
+  paidOverBillExpenses: paidOverBill[] = [];
 
 
   /**
@@ -208,16 +222,67 @@ export class CalendarComponent implements OnInit {
 
     this.year = this.year + interval;
 
-
-    this.setUpCalendar(this.displayedMonth);
-
+    this.initCalendar();
     // this.changeYr.emit(this.year);
   }
 
+  private getExpenses() {
+    this.paidOverBillExpenses = [];
+
+    this.serviceExpenses.getExpensesOf(this.displayedMonth+1,this.year,"date").subscribe(expenses => {
+      for (let expense of expenses){
+        this.paidOverBillExpenses.push(new paidOverBill(expense));
+      }
+    });
+  }
+
+
+  private getInfObj(stringDate: string) {
+    for (let obj of this.paidOverBillExpenses){
+      let tsts = stringDate == obj.strDate;
+      if (tsts){
+        return obj;
+      }
+    }
+    return {strDate: null, bill: false, paid: false, over: false, expense: null};
+  }
+
+  private initCalendar(reset?: boolean) {
+    let month = reset === true ?  this.currentMonth : this.displayedMonth;
+
+    this.getExpenses();
+    setTimeout(()=>{
+      this.setUpCalendar(month);
+    }, 500);
+
+  }
 }
-// display: inline-block;
-// vertical-align: top;
-// border-collapse: collapse;
-// width: 80%;
-// margin: 0 auto;
-// height: 200px;
+
+
+
+export class paidOverBill{
+  strDate: string;
+
+  bill:boolean;
+  paid:boolean;
+  over:boolean;
+
+  expense: Expense;
+
+  constructor(expense: Expense){
+    if (expense.date != null ) {
+      this.strDate = new Date(expense.date).toISOString().slice(0,10);
+    }
+
+    this.bill = true;
+    this.paid = expense.alreadyPaid === 1;
+    this.over = !expense.alreadyPaid && expense.date > new Date();
+
+    this.expense = expense;
+  }
+
+  toString():string{
+    return this.expense.id+" on "+this.strDate+"> Paid:"+this.paid+" Over:"+this.over;
+  }
+
+}
